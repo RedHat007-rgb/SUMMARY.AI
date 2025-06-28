fa88250e58a59963c2725833e53efc05e25ab218
RedHat007-rgb <cu.16bcs1288@gmail.com>
2025-06-28 12:48:59 +0530

    Added COnnect function

--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,22 @@
 const express = require("express");
 const authRouter = require("./routes/authroute");
+const { dbConnect } = require("./db/db");
 const app = express();
 require("dotenv").config();
 const PORT = process.env.PORT;
-
+const MONGO_URL = process.env.MONGO_URL;
+app.use(express.json());
 app.use("/api/v1/auth", authRouter);
 
-app.listen(process.env.PORT, () => {
-  console.log(`server is listening to ${PORT}`);
-});
+const connect = async () => {
+  try {
+    await dbConnect(MONGO_URL);
+    app.listen(process.env.PORT, () => {
+      console.log(`server is listening to ${PORT}`);
+    });
+  } catch {
+    console.log("Internal server error");
+  }
+};
+
+connect();
