cefff11dd95286f1261ff7983dac1a9ba41b0b9f
RedHat007-rgb <cu.16bcs1288@gmail.com>
2025-06-28 15:12:03 +0530

    test

--- backend/routes/authroute.js
+++ backend/routes/authroute.js
@@ -1,10 +1,18 @@
 const express = require("express");
-const { signupHandler, loginHandler } = require("../controllers/authHandler");
+const {
+  signupHandler,
+  loginHandler,
+  testHandler,
+} = require("../controllers/authHandler");
+const { authMiddleware } = require("../middlewares/authMiddleware");
 
 const authRouter = express.Router();
 
 authRouter.route("/signup").post(signupHandler);
 
 authRouter.route("/login").post(loginHandler);
+
+authRouter.use(authMiddleware);
+authRouter.route("/test").get(testHandler);
 
 module.exports = authRouter;
