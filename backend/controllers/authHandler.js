45b9058321d2be7d3e4c0d073dab79dc82c65f86
RedHat007-rgb <cu.16bcs1288@gmail.com>
2025-06-28 15:11:44 +0530

    Implemented login handler

--- backend/controllers/authHandler.js
+++ backend/controllers/authHandler.js
@@ -1,35 +1,74 @@
 const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 const { userModel } = require("../models/userModel");
 require("dotenv").config();
+
 const signupHandler = async (req, res) => {
-  console.log(req.body);
   try {
-    console.log("inside signup....");
     const { email, firstName, lastName, password } = req.body;
-    console.log(req.body);
-    const duplicateUser = await userModel.findOne({ email: email });
+    const duplicateUser = await userModel.findOne({ email });
     if (duplicateUser) {
       return res.status(409).json({
-        message: "EmailID already exists.Please use another EmailId",
+        message: "Email ID already exists. Please use another Email ID.",
       });
     }
-    bcrypt.hash(
-      password,
-      parseInt(process.env.SALT_ROUNDS),
-      async function (err, hash) {
-        if (hash) {
-          await userModel.create({
-            email,
-            firstName,
-            lastName,
-            hash,
-          });
-        }
-        res.status(200).json({
-          message: "Account Created...",
-        });
-      }
-    );
+    const SALT_ROUNDS = parseInt(process.env.SALT_ROUNDS);
+    const hashedPassword = await bcrypt.hash(password, SALT_ROUNDS);
+    await userModel.create({
+      email,
+      firstName,
+      lastName,
+      password: hashedPassword,
+    });
+    return res.status(201).json({
+      message: "Account created successfully.",
+    });
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({
+      message: "Internal server error",
+    });
+  }
+};
+
+const loginHandler = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    console.log(req.body);
+    console.log(email);
+    if (!email || !password) {
+      return res.status(400).json({
+        message: "Email and password are required",
+      });
+    }
+    const foundUser = await userModel.findOne({ email: email });
+    console.log(foundUser);
+    if (!foundUser) {
+      return res.status(404).json({
+        message: "Please enter valid EmailId",
+      });
+    }
+    const decryptPassword = await bcrypt.compare(password, foundUser.password);
+    console.log(decryptPassword);
+    if (!decryptPassword) {
+      return res.status(401).json({
+        message: "Please enter correct password.",
+      });
+    }
+    try {
+      const token = jwt.sign({ userId: foundUser._id }, process.env.JWT_KEY, {
+        expiresIn: "1h",
+      });
+
+      return res.status(200).setHeader("token", `Bearer ${token}`).json({
+        message: "Successfully logged in",
+      });
+    } catch (jwtError) {
+      console.error("JWT generation error:", jwtError);
+      return res.status(500).json({
+        message: "Failed to generate authentication token",
+      });
+    }
   } catch {
     res.status(500).json({
       message: "Internal server error",
@@ -37,10 +76,8 @@ const signupHandler = async (req, res) => {
   }
 };
 
-const loginHandler = (req, res) => {
-  res.status(200).json({
-    message: "Successfully lopggedin...",
-  });
+const testHandler = (req, res) => {
+  res.json("in test");
 };
 
-module.exports = { loginHandler, signupHandler };
+module.exports = { loginHandler, signupHandler, testHandler };
